f8bdca9a5c7e514f27d8de4581a552e2d1097203
Kushal Vadodaria <kushalv238@gmail.com>
2022-04-12 19:11:39 +0530

    Commit 4
    -Code efficiency improved
    -Whitespace changes

--- script/script.js
+++ script/script.js
@@ -16,24 +16,27 @@ const game = function(input){
     document.getElementById('comp-choice').textContent = `${compCh}`;
 
     const result = getStatus(userCh, compCh);
-    document.getElementById('result').textContent = `${result}`;    
+    document.getElementById('result').textContent = `${result}`;
     console.log(result);
 }
 
 function getStatus(userCh_, compCh_) {
-    if(userCh_ === compCh_) {
-        document.getElementById('result').style.color = 'yellow';
-        return("It's a draw!");
-    }
-    else if((userCh_ === 'rock' && compCh_ === 'scissor') || (userCh_ === 'paper' && compCh_ === 'rock') || (userCh_ === 'scissor' && compCh_ === 'paper')) {
+    if((userCh_ === 'rock' && compCh_ === 'scissor') || (userCh_ === 'paper' && compCh_ === 'rock') || (userCh_ === 'scissor' && compCh_ === 'paper')) {
         document.getElementById('user-score').textContent++;
         document.getElementById('result').style.color = 'green';
         return("You win!");
     }
-    else {
+    else if((userCh_ === 'rock' && compCh_ === 'paper') || (userCh_ === 'paper' && compCh_ === 'scissor') || (userCh_ === 'scissor' && compCh_ === 'rock')) {
         document.getElementById('comp-score').textContent++;
         document.getElementById('result').style.color = 'red';
         return("You loose!");
+    }
+    else if(userCh_ == compCh_){
+        document.getElementById('result').style.color = 'yellow';
+        return("It's a draw!");
+    }
+    else {
+        location.reload();
     }
 }
 
@@ -54,13 +57,13 @@ function finalRes() {
 }
 
 function finalResValue(US, CS) {
-    if(US>CS) {
+    if(US > CS) {
         return("Wait how did you win, you probably cheated!");
     }
-    else if(US<CS) {
+    else if(US < CS) {
         return("To no one's surprise, I won ;)");
     }
-    else{
+    else {
         return("Well I guess we are equally bad :p");
     }
 }
